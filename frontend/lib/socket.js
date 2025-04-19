6b9559bb9791f2b0b43795f819f9822e77d02f4a
CJRiney <chrisriney4@gmail.com>
2025-04-19 14:18:17 -0700

    chris - just fixing small socket bug

--- frontend/lib/socket.js
+++ frontend/lib/socket.js
@@ -1,6 +1,6 @@
-import Constants from 'expo-constants';
+import { io } from "socket.io-client"
 
-const API_URL = Constants.expoConfig.extra.EXPO_API_URL;
+const API_URL = "http://10.0.0.242:8888";
 const socket = io(API_URL, {
   transports: ['websocket'],  // force WebSocket only (skip long-polling)
   withCredentials: true       // allows cookie/session-based auth
