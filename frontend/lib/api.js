5fd7aabc9f07dca7cce620f27e5f75d9d9c23fe9
CJRiney <chrisriney4@gmail.com>
2025-04-21 15:11:21 -0700

    chris - can now save recipes

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://10.0.0.242:8888";
+const API_URL = "https://cully-4gu8.onrender.com";
 
 export const signup = async(user) => {
     const res = await fetch(`${API_URL}/signup`, {
