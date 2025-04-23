2e3b59f210ec86741dbacbddf03f343fb550f91d
CJRiney <chrisriney4@gmail.com>
2025-04-23 07:50:19 -0700

    really annoying stuff

---
+++ frontend/app/(safe)/safepage.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SafeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello from Safe Screen</Text>
+    </View>
+  );
+}
