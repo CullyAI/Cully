28c5b402274e4cf6d9f86fb44f7bc821b6cb0615
CJRiney <chrisriney4@gmail.com>
2025-04-23 08:13:41 -0700

    chris - might be working

--- frontend/app/(safe)/safepage.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function SafeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello from Safe Screen</Text>
-    </View>
-  );
-}
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -17,41 +17,35 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-// export default function RootLayout() {
-//   const colorScheme = useColorScheme();
-//   const [loaded] = useFonts({
-//     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-//   });
-
-//   useEffect(() => {
-//     if (loaded) {
-//       SplashScreen.hideAsync();
-//     }
-//   }, [loaded]);
-
-//   if (!loaded) {
-//     return null;
-//   }
-
-//   return (
-//     <AuthProvider>
-//       <NavProvider>
-//         <ThemeProvider
-//           value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-//         >
-//           <Stack>
-//             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-//             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-//           </Stack>
-//           <StatusBar style="auto" />
-//         </ThemeProvider>
-//       </NavProvider>
-//     </AuthProvider>
-//   );
-// }
-
-import SafeScreen from "./(safe)/safepage";
-
 export default function RootLayout() {
-  return <SafeScreen />;
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <AuthProvider>
+      <NavProvider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </NavProvider>
+    </AuthProvider>
+  );
 }
