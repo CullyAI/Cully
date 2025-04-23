449ac3848fc8e568a2992557182432d18ec0918d
CJRiney <chrisriney4@gmail.com>
2025-04-23 08:03:59 -0700

    chris - testing witha  safescreen

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -17,35 +17,41 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+// export default function RootLayout() {
+//   const colorScheme = useColorScheme();
+//   const [loaded] = useFonts({
+//     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+//   });
+
+//   useEffect(() => {
+//     if (loaded) {
+//       SplashScreen.hideAsync();
+//     }
+//   }, [loaded]);
+
+//   if (!loaded) {
+//     return null;
+//   }
+
+//   return (
+//     <AuthProvider>
+//       <NavProvider>
+//         <ThemeProvider
+//           value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+//         >
+//           <Stack>
+//             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+//             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+//           </Stack>
+//           <StatusBar style="auto" />
+//         </ThemeProvider>
+//       </NavProvider>
+//     </AuthProvider>
+//   );
+// }
+
+import SafeScreen from "./(safe)/safepage";
+
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <AuthProvider>
-      <NavProvider>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </NavProvider>
-    </AuthProvider>
-  );
+  return <SafeScreen />;
 }
